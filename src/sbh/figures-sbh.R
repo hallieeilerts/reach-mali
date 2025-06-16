0801c8f71c23192104a7fe1c4f9ae05378573bc3
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-06-16 11:59:51 -0400

    experimental sbh plots

--- src/sbh/figures-sbh.R
+++ src/sbh/figures-sbh.R
@@ -9,10 +9,14 @@ library(tidyr)
 library(dplyr)
 library(viridis)
 library(cowplot)
+library(ggpubr)
 #' Inputs
 source("./src/utils.R")
 dat <- readRDS("./gen/sbh/temp/sbh-qsecover-qwsec01.rds")
 ################################################################################
+
+
+# Children born by respondent age -----------------------------------------
 
 dat %>%
   group_by(agecat_resp, q208) %>%
@@ -21,7 +25,7 @@ dat %>%
   geom_bar(aes(x=q208, y = n), stat = "identity") +
   facet_wrap(~agecat_resp)
 
-# Children born by age ----------------------------------------------------
+# Children born by age of mother ----------------------------------------------------
 
 p <- dat %>%
   group_by(agecat_resp, q208) %>%
@@ -55,7 +59,58 @@ dat %>%
 # Figure~\ref{fig:sbh-children-born-byage} shows the total number of children born by respondent age. In the summary birth history, this is a sum of reported children living with the respondent, living elsewhere, and deceased. It does not include pregnancy losses or stillbirths (unless reported as part of children deceased). The distribution looks reasonable with the vast majority of respondents aged 15-19 having only 1 birth, and the distributions gradually shifting rightward with age, reflecting higher parity among older respondents.
 
 
-# Combined plot for pregnancies by age ------------------------------------
+# Proportion of children ever born/died by age of mother ------------------
+
+# q208 is total children born
+# q205_comb is total living at home
+# q205_comb is total living elsewhere
+dat %>%
+  filter(q208 != 0) %>%
+  mutate(propsurv = (q203_comb + q205_comb)/q208) %>%
+  group_by(agecat_resp) %>%
+  summarise(avgsurv = sprintf("%0.1f", round(mean(propsurv)*100, 1))) %>%
+  kbl(format = "latex", booktabs = TRUE, row.names = FALSE, linesep = "",
+      format.args = list(big.mark = ",", scientific = FALSE), 
+      col.names = c("Mother age", "%"), 
+      caption = "Average proportion children surviving by age of mother.") 
+
+# q208 is total children born
+# q207_comb is total died
+dat %>%
+  filter(q208 != 0) %>%
+  mutate(propsurv = (q207_comb)/q208) %>%
+  group_by(agecat_resp) %>%
+  summarise(avgsurv = sprintf("%0.1f", round(mean(propsurv)*100, 1))) %>%
+  kbl(format = "latex", booktabs = TRUE, row.names = FALSE, linesep = "",
+      format.args = list(big.mark = ",", scientific = FALSE), 
+      col.names = c("Mother age", "%"), 
+      label = c("sbh-propdied"),
+      caption = "Average proportion children died by age of mother.") 
+
+# histogram
+dat %>%
+  filter(q208 != 0) %>%
+  mutate(propdied = q207_comb / q208) %>%
+  filter(propdied <= 1) %>%
+  ggplot(aes(x = propdied)) +
+  geom_histogram(fill = "#0D0887FF") +
+  facet_wrap(~agecat_resp) +
+  theme_bw() +
+  theme(text = element_text(size = 10), title = element_text(size = 8))
+#ggsave("./gen/fph/figures/cebcd-matage.png", p, dpi = 300, width = 6, height = 6)
+
+# density plot
+dat %>%
+  filter(q208 != 0) %>%
+  mutate(propdied = q207_comb / q208) %>%
+  filter(propdied <= 1) %>%
+  ggplot(aes(x = propdied)) +
+  geom_density(fill = "#0D0887FF") +
+  facet_wrap(~agecat_resp, scales = "free_y") +
+  labs(x = "", y = "Density") +
+  theme_minimal()
+
+# Combined plot for pregnancies by age of mother ------------------------------------
 
 # Bar plot with legend
 p1_full <- dat %>%
@@ -145,6 +200,11 @@ final_plot <- ggarrange(
 # Save the final figure
 ggsave("./gen/sbh/figures/sbh-pregnancies-byage-combined.png",
        final_plot, dpi = 300, width = 6, height = 3)
+
+
+# Combined plot for number of births by age of mother ---------------------
+
+
 
 
 # Combined plot for children born by age ----------------------------------
@@ -243,12 +303,13 @@ ggsave("./gen/sbh/figures/sbh-children-born-byage-combined.png",
 
 # Bar plot with legend
 p1_full <- dat %>%
-  group_by(agecat_resp, q206) %>%
+  filter(!is.na(q207_comb) & q207_comb != 0) %>%
+  group_by(agecat_resp, q207_comb) %>%
   summarise(n = n()) %>%
   ggplot() +
-  geom_bar(aes(x=q206, y = n, fill = agecat_resp), position = "dodge", stat = "identity") +
+  geom_bar(aes(x=q207_comb, y = n, fill = agecat_resp), position = "dodge", stat = "identity") +
   scale_fill_viridis_d(option = "C", name = "Âge")  +
-  scale_x_continuous(breaks = c(1,2)) +
+  scale_x_continuous(breaks = 1:10) +
   labs(
     #title = "Nombre total d'enfants décédés selon l’âge des enquêtées",
     subtitle = "a",
