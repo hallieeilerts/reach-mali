8a50f6fd296d3042c2f514d15fcb0ba7a6ad4899
hallieeilerts <60435505+hallieeilerts@users.noreply.github.com>
2025-07-01 10:34:46 -0400

    added sections for denombrement and mapping

--- make.R
+++ make.R
@@ -29,46 +29,36 @@ source("./src/dhs/qc-gr.R", local = new.env())
 source("./src/dhs/combine-qc.R", local = new.env())
 source("./src/dhs/table-dhs.R", local = new.env())
 source("./src/dhs/figure-table-prep-dhs-mali.R", local = new.env())
+source("./src/dhs/calc-demogsurv-dhs-mali.R", local = new.env())
+source("./src/dhs/calc-gapu5m-dhs-mali.R", local = new.env())
 
-# Cover and section 1 -----------------------------------------------------
+# New instat files --------------------------------------------------------
 
-source("./src/sec01/explore-qsecover.R", local = new.env())
+source("./src/combined/explore-partb-womens.R", local = new.env())
+
+# Women's questionnaire: cover and section 1 ------------------------------
+
+source("./src/sec01/clean-level1.R", local = new.env())
 source("./src/sec01/clean-qsecover.R", local = new.env())
-source("./src/sec01/explore-qwsec01.R", local = new.env())
 source("./src/sec01/clean-qwsec01.R", local = new.env())
-#source("./src/sec01/impute-respondent-dob.R", local = new.env())
 source("./src/sec01/impute-respondent-age.R", local = new.env())
 source("./src/sec01/combine-cover-sec01.R", local = new.env())
 source("./src/sec01/figures-cover-sec01.R", local = new.env())
 
-# Mortality dénombrement --------------------------------------------------
-
-source("./src/denomb-mort/figures-denomb-mort.R", local = new.env())
-
-# Need to create table for number of women in denombrement
-# number of women 15-49 in denombrement
-# number of women 15-49 in denombrement in mortality survey
-# number per cluster/region
-# I don't think that I can do this currently. 
-# The individual IDs that come with reach_mortalite_femme_qsecover.csv are...
-# qsecover_id, level_1_id, w_nom and more household variables..
-# Those in reach_mortalite_denomb_qhhsecover are...
-# qhhsecover_id level_1_id qhhi1 and more household variables...
-# I don't think these level 1 IDs match though
-
-# SBH ---------------------------------------------------------------------
+# Women's questionnaire: SBH -------------------------------------
 
 source("./src/sbh/explore-sbh.R", local = new.env())
-source("./src/sbh/clean-sbh.R", local = new.env())
 source("./src/sbh/combine-sbh-qsecover.R", local = new.env())
+source("./src/sbh/clean-sbh.R", local = new.env())
 source("./src/sbh/figures-sbh.R", local = new.env())
 
-# FPH ---------------------------------------------------------------------
+# Women's questionnaire: FPH -------------------------------------
 
 source("./src/fph/explore-fph.R", local = new.env())
 source("./src/fph/clean-fph.R", local = new.env())
 source("./src/fph/audit-dob.R", local = new.env())
 source("./src/fph/combine-fph-qsecover.R", local = new.env())
+source("./src/fph/recover-doby.R", local = new.env())
 source("./src/fph/clean-aod.R", local = new.env())
 source("./src/fph/impute-dob.R", local = new.env())
 source("./src/fph/add-tips-period.R", local = new.env())
@@ -85,4 +75,15 @@ source("./src/mort/calc-demogsurv.R", local = new.env())
 source("./src/mort/calc-dhsrates.R", local = new.env())
 source("./src/mort/compare-methods.R", local = new.env())
 source("./src/mort/figures-mort.R", local = new.env())
+source("./src/mort/figures-mort-mldhs.R", local = new.env())
+
+# Dénombrement --------------------------------------------------
+
+source("./src/denomb/combine-qhhsecover-qhsec01.R", local = new.env())
+source("./src/denomb/figures-denomb-mort.R", local = new.env())
+
+# Mapping (mortalite-menage) -------------------------------------
+
+source("./src/mapping/combine-qhsec01-hhrec.R", local = new.env())
+source("./src/mapping/table-data-collection.R", local = new.env())
 
